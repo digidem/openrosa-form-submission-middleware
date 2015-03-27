757026c159c6c47db9bcf1d1438f2fec05741532
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-03-27 16:14:32 -0700

    Switch to JS Standard style

--- index.js
+++ index.js
@@ -10,12 +10,12 @@
  * Module dependencies.
  */
 
-var multiparty = require('multiparty');
-var onFinished = require('on-finished');
-var typeis = require('type-is');
-var fs = require('fs');
-var openrosaRequest = require('openrosa-request-middleware');
-var openrosaRequestMiddleware = openrosaRequest();
+var multiparty = require('multiparty')
+var onFinished = require('on-finished')
+var typeis = require('type-is')
+var fs = require('fs')
+var openrosaRequest = require('openrosa-request-middleware')
+var openrosaRequestMiddleware = openrosaRequest()
 
 /**
  * Multipart:
@@ -30,103 +30,106 @@ var openrosaRequestMiddleware = openrosaRequest();
  *  `Form` object, allowing you to configure the upload directory,
  *  size limits, etc. For example if you wish to change the upload dir do the following.
  *
- *     app.use(connect.multipart({ uploadDir: path }));
+ *     app.use(connect.multipart({ uploadDir: path }))
  *
  * @param {Object} options
  * @return {Function}
  * @api public
  */
 
-module.exports = function(options){
-  options = options || {};
-  options.maxContentLength = options.maxContentLength || 10485760;
+module.exports = function (options) {
+  options = options || {}
+  options.maxContentLength = options.maxContentLength || 10485760
 
-  function multipart(req, res, next) {
-    if (req._body) return next();
-    req.files = [];
-    req.body = undefined;
+  function multipart (req, res, next) {
+    if (req._body) return next()
+    req.files = []
+    req.body = undefined
 
-    // Response to HEAD requests with status 204
-    if ('HEAD' === req.method) return res.sendStatus(204);
-    // Ignore all other request methods other than post
-    else if ('POST' !== req.method) return next();
+    if (req.method === 'HEAD') {
+      // Response to HEAD requests with status 204
+      return res.sendStatus(204)
+    } else if (req.method !== 'POST') {
+      // Ignore all other request methods other than post
+      return next()
+    }
 
     // check Content-Type
-    if (!typeis(req, 'multipart/form-data')) return next();
+    if (!typeis(req, 'multipart/form-data')) return next()
 
     // flag as parsed
-    req._body = true;
+    req._body = true
 
     // parse
-    var form = new multiparty.Form(options);
-    var done = false;
-    var processingXml = false;
-    var wasError = false;
+    var form = new multiparty.Form(options)
+    var done = false
+    var processingXml = false
+    var wasError = false
 
-    form.on('file', function(name, val){
-      val.name = val.originalFilename;
-      val.type = val.headers['content-type'] || null;
+    form.on('file', function (name, val) {
+      val.name = val.originalFilename
+      val.type = val.headers['content-type'] || null
 
       if (name === 'xml_submission_file') {
-        processingXml = true;
-        fs.readFile(val.path, function(err, data) {
-          if (err) onError(err);
-          req.body = data.toString();
-          fs.unlink(val.path, function(err) {
-            if (err) console.error('Error deleting file %s', val.path);
-          });
-          processingXml = false;
-          if (done && !wasError) next();
-        });
+        processingXml = true
+        fs.readFile(val.path, function (err, data) {
+          if (err) onError(err)
+          req.body = data.toString()
+          fs.unlink(val.path, function (err) {
+            if (err) console.error('Error deleting file %s', val.path)
+          })
+          processingXml = false
+          if (done && !wasError) next()
+        })
 
       } else {
-        req.files.push(val);
+        req.files.push(val)
       }
-    });
+    })
 
-    form.on('error', function(err) {
-      if (done) return;
-      onError(err);
-    });
+    form.on('error', function (err) {
+      if (done) return
+      onError(err)
+    })
 
-    form.on('close', function() {
-      if (done) return;
+    form.on('close', function () {
+      if (done) return
 
-      done = true;
+      done = true
 
       // only continue if we have already processed the xml submission file
       // and attached it to the req.body
       if (req.body) {
-        next();
+        next()
       } else if (!processingXml) {
-        onError(new Error('No xml submission file included in request'));
+        onError(new Error('No xml submission file included in request'))
       }
-    });
+    })
 
-    function onError(err) {
-      done = wasError = true;
+    function onError (err) {
+      done = wasError = true
 
-      err.status = 400;
+      err.status = 400
 
-      if (!req.readable) return next(err);
+      if (!req.readable) return next(err)
 
-      req.resume();
-      onFinished(req, function(){
-        next(err);
-      });
+      req.resume()
+      onFinished(req, function () {
+        next(err)
+      })
     }
 
-    form.parse(req);
+    form.parse(req)
   }
 
-  return function(req, res, next) {
+  return function (req, res, next) {
     // Set correct OpenRosa headers
     // see https://bitbucket.org/javarosa/javarosa/wiki/OpenRosaRequest
     // and https://bitbucket.org/javarosa/javarosa/wiki/FormSubmissionAPI
-    openrosaRequestMiddleware(req, res, function(err) {
-      if (err) next(err);
-      res.setHeader('X-OpenRosa-Accept-Content-Length', options.maxContentLength);
-      multipart(req, res, next);
-    });
-  };
-};
+    openrosaRequestMiddleware(req, res, function (err) {
+      if (err) next(err)
+      res.setHeader('X-OpenRosa-Accept-Content-Length', options.maxContentLength)
+      multipart(req, res, next)
+    })
+  }
+}
--- test/openrosa-form-submission.js
+++ test/openrosa-form-submission.js
@@ -1,277 +1,277 @@
-process.env.NODE_ENV = 'test';
+/*global describe it*/
+process.env.NODE_ENV = 'test'
 
-var express = require('express');
-var formSubmission = require('..');
-var request = require('supertest');
-var should = require('should');
-var fs = require('fs');
-var path = require('path');
+var express = require('express')
+var formSubmission = require('..')
+var request = require('supertest')
+var fs = require('fs')
+var path = require('path')
 
-var app = express();
+require('should')
 
-app.use(formSubmission());
+var app = express()
 
-app.use(function(req, res){
-  res.end(JSON.stringify(req.body));
-});
+app.use(formSubmission())
 
-describe('formSubmissionMiddleware()', function(){
-  it('should ignore GET', function(done){
+app.use(function (req, res) {
+  res.end(JSON.stringify(req.body))
+})
+
+describe('formSubmissionMiddleware()', function () {
+  it('should ignore GET', function (done) {
     request(app)
-    .get('/')
-    .set('X-OpenRosa-Version', '1.0')
-    .field('user', 'Tobi')
-    .expect(200, '', done);
+      .get('/')
+      .set('X-OpenRosa-Version', '1.0')
+      .field('user', 'Tobi')
+      .expect(200, '', done)
   })
 
-  it('should respond to HEAD with 204 and X-OpenRosa-Version header', function(done){
+  it('should respond to HEAD with 204 and X-OpenRosa-Version header', function (done) {
     request(app)
-    .head('/')
-    .set('X-OpenRosa-Version', '1.0')
-    .field('user', 'Tobi')
-    .expect('X-OpenRosa-Version', '1.0')
-    .expect(204, '', done);
+      .head('/')
+      .set('X-OpenRosa-Version', '1.0')
+      .field('user', 'Tobi')
+      .expect('X-OpenRosa-Version', '1.0')
+      .expect(204, '', done)
   })
 
-  describe('with multipart/form-data', function(){
+  describe('with multipart/form-data', function () {
+    var xmlFixture = fs.readFileSync(path.join(__dirname, 'fixtures', 'xml_submission_file.xml'))
 
-    var xmlFixture = fs.readFileSync(path.join(__dirname, 'fixtures', 'xml_submission_file.xml'));
+    it('should return xml_submission_file as the form body', function (done) {
+      var app = express()
 
-    it('should return xml_submission_file as the form body', function(done){
-      var app = express();
+      app.use(formSubmission())
 
-      app.use(formSubmission());
-
-      app.use(function(req, res){
-        req.body.should.eql(xmlFixture.toString());
-        req.files.length.should.eql(0);
-        res.end('{}');
-      });
-
-      request(app)
-      .post('/')
-      .set('X-OpenRosa-Version', '1.0')
-      .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
-      .expect(200, '{}', done);
-    })
-    
-    it('should handle multiple file attachments', function(done){
-      var app = express();
-
-      app.use(formSubmission());
-
-      app.use(function(req, res){
-        req.body.should.eql(xmlFixture.toString());
-        req.files[0].path.should.endWith('.jpg');
-        req.files.length.should.eql(2);
-        res.end(req.files[1].originalFilename);
-      });
+      app.use(function (req, res) {
+        req.body.should.eql(xmlFixture.toString())
+        req.files.length.should.eql(0)
+        res.end('{}')
+      })
 
       request(app)
-      .post('/')
-      .set('X-OpenRosa-Version', '1.0')
-      .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
-      .attach('photo', path.join(__dirname, 'fixtures', 'image.jpg'))
-      .attach('photo', path.join(__dirname, 'fixtures', 'image2.jpg'))
-      .expect(200, 'image2.jpg', done);
+        .post('/')
+        .set('X-OpenRosa-Version', '1.0')
+        .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
+        .expect(200, '{}', done)
     })
 
-    it('should support multiple files of the same name', function(done){
-      var app = express();
+    it('should handle multiple file attachments', function (done) {
+      var app = express()
 
-      app.use(formSubmission());
+      app.use(formSubmission())
 
-      app.use(function(req, res){
-        req.files.should.have.length(2);
-        req.files[0].constructor.name.should.equal('Object');
-        req.files[1].constructor.name.should.equal('Object');
-        res.end();
-      });
+      app.use(function (req, res) {
+        req.body.should.eql(xmlFixture.toString())
+        req.files[0].path.should.endWith('.jpg')
+        req.files.length.should.eql(2)
+        res.end(req.files[1].originalFilename)
+      })
 
       request(app)
-      .post('/')
-      .set('X-OpenRosa-Version', '1.0')
-      .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
-      .attach('text', new Buffer('some text here'), { filename: 'foo.txt' })
-      .attach('text', new Buffer('some more text stuff'), { filename: 'bar.txt' })
-      .expect(200, done);
+        .post('/')
+        .set('X-OpenRosa-Version', '1.0')
+        .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
+        .attach('photo', path.join(__dirname, 'fixtures', 'image.jpg'))
+        .attach('photo', path.join(__dirname, 'fixtures', 'image2.jpg'))
+        .expect(200, 'image2.jpg', done)
     })
-    
-    it('should support nested files', function(done){
-      var app = express();
 
-      app.use(formSubmission());
+    it('should support multiple files of the same name', function (done) {
+      var app = express()
 
-      app.use(function(req, res){
-        req.files.should.have.length(2);
-        req.files[0].originalFilename.should.equal('foo.txt');
-        req.files[1].originalFilename.should.equal('bar.txt');
-        res.end();
-      });
+      app.use(formSubmission())
+
+      app.use(function (req, res) {
+        req.files.should.have.length(2)
+        req.files[0].constructor.name.should.equal('Object')
+        req.files[1].constructor.name.should.equal('Object')
+        res.end()
+      })
 
       request(app)
-      .post('/')
-      .set('X-OpenRosa-Version', '1.0')
-      .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
-      .attach('docs[foo]', new Buffer('some text here'), { filename: 'foo.txt' })
-      .attach('docs[bar]', new Buffer('some more text stuff'), { filename: 'bar.txt' })
-      .expect(200, done);
-    })
-    
-    it('should next(err) on multipart failure', function(done){
-      var app = express();
-
-      app.use(formSubmission());
-
-      app.use(function(req, res){
-        res.end('whoop');
-      });
-
-      app.use(function(err, req, res, next){
-        err.message.should.equal('Expected alphabetic character, received 61');
-        res.statusCode = err.status;
-        res.end('bad request');
-      });
-
-      var test = request(app).post('/').set('X-OpenRosa-Version', '1.0');
-      test.set('Content-Type', 'multipart/form-data; boundary=foo');
-      test.write('--foo\r\n');
-      test.write('Content-filename="foo.txt"\r\n');
-      test.write('\r\n');
-      test.write('some text here');
-      test.write('Content-Disposition: form-data; name="text"; filename="bar.txt"\r\n');
-      test.write('\r\n');
-      test.write('some more text stuff');
-      test.write('\r\n--foo--');
-      test.expect(400, 'bad request', done);
+        .post('/')
+        .set('X-OpenRosa-Version', '1.0')
+        .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
+        .attach('text', new Buffer('some text here'), { filename: 'foo.txt' })
+        .attach('text', new Buffer('some more text stuff'), { filename: 'bar.txt' })
+        .expect(200, done)
     })
 
-    it('should not hang request on failure', function(done){
-      var app = express();
-      var buf = new Buffer(1024 * 10);
+    it('should support nested files', function (done) {
+      var app = express()
 
-      app.use(formSubmission());
+      app.use(formSubmission())
 
-      app.use(function(req, res){
-        res.end('whoop');
-      });
-
-      app.use(function(err, req, res, next){
-        err.message.should.equal('Expected alphabetic character, received 61');
-        res.statusCode = err.status;
-        res.end('bad request');
-      });
-
-      buf.fill('.');
-
-      var test = request(app).post('/').set('X-OpenRosa-Version', '1.0');
-      test.set('Content-Type', 'multipart/form-data; boundary=foo');
-      test.write('--foo\r\n');
-      test.write('Content-filename="foo.txt"\r\n');
-      test.write('\r\n');
-      test.write('some text here');
-      test.write('Content-Disposition: form-data; name="text"; filename="bar.txt"\r\n');
-      test.write('\r\n');
-      test.write('some more text stuff');
-      test.write('\r\n--foo--');
-      test.write(buf);
-      test.write(buf);
-      test.write(buf);
-      test.expect(400, 'bad request', done);
-    })
-
-    it('should default req.files to []', function(done){
-      var app = express();
-
-      app.use(formSubmission());
-
-      app.use(function(req, res){
-        res.end(JSON.stringify(req.files));
-      });
+      app.use(function (req, res) {
+        req.files.should.have.length(2)
+        req.files[0].originalFilename.should.equal('foo.txt')
+        req.files[1].originalFilename.should.equal('bar.txt')
+        res.end()
+      })
 
       request(app)
-      .post('/')
-      .set('X-OpenRosa-Version', '1.0')
-      .expect(200, '[]', done);
+        .post('/')
+        .set('X-OpenRosa-Version', '1.0')
+        .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
+        .attach('docs[foo]', new Buffer('some text here'), { filename: 'foo.txt' })
+        .attach('docs[bar]', new Buffer('some more text stuff'), { filename: 'bar.txt' })
+        .expect(200, done)
     })
 
-    it('should return 400 on maxFilesSize exceeded', function(done){
-      var app = express();
+    it('should next(err) on multipart failure', function (done) {
+      var app = express()
 
-      var exp = 9;
-      app.use(formSubmission({ maxFilesSize: Math.pow(2, exp) }));
+      app.use(formSubmission())
 
-      app.use(function(req, res){
-        res.end(JSON.stringify(req.files));
-      });
+      app.use(function (req, res) {
+        res.end('whoop')
+      })
 
-      var str = 'x';
+      app.use(function (err, req, res, next) {
+        err.message.should.equal('Expected alphabetic character, received 61')
+        res.statusCode = err.status
+        res.end('bad request')
+      })
+
+      var test = request(app).post('/').set('X-OpenRosa-Version', '1.0')
+      test.set('Content-Type', 'multipart/form-data; boundary=foo')
+      test.write('--foo\r\n')
+      test.write('Content-filename="foo.txt"\r\n')
+      test.write('\r\n')
+      test.write('some text here')
+      test.write('Content-Disposition: form-data; name="text"; filename="bar.txt"\r\n')
+      test.write('\r\n')
+      test.write('some more text stuff')
+      test.write('\r\n--foo--')
+      test.expect(400, 'bad request', done)
+    })
+
+    it('should not hang request on failure', function (done) {
+      var app = express()
+      var buf = new Buffer(1024 * 10)
+
+      app.use(formSubmission())
+
+      app.use(function (req, res) {
+        res.end('whoop')
+      })
+
+      app.use(function (err, req, res, next) {
+        err.message.should.equal('Expected alphabetic character, received 61')
+        res.statusCode = err.status
+        res.end('bad request')
+      })
+
+      buf.fill('.')
+
+      var test = request(app).post('/').set('X-OpenRosa-Version', '1.0')
+      test.set('Content-Type', 'multipart/form-data; boundary=foo')
+      test.write('--foo\r\n')
+      test.write('Content-filename="foo.txt"\r\n')
+      test.write('\r\n')
+      test.write('some text here')
+      test.write('Content-Disposition: form-data; name="text"; filename="bar.txt"\r\n')
+      test.write('\r\n')
+      test.write('some more text stuff')
+      test.write('\r\n--foo--')
+      test.write(buf)
+      test.write(buf)
+      test.write(buf)
+      test.expect(400, 'bad request', done)
+    })
+
+    it('should default req.files to []', function (done) {
+      var app = express()
+
+      app.use(formSubmission())
+
+      app.use(function (req, res) {
+        res.end(JSON.stringify(req.files))
+      })
+
+      request(app)
+        .post('/')
+        .set('X-OpenRosa-Version', '1.0')
+        .expect(200, '[]', done)
+    })
+
+    it('should return 400 on maxFilesSize exceeded', function (done) {
+      var app = express()
+
+      var exp = 9
+      app.use(formSubmission({ maxFilesSize: Math.pow(2, exp) }))
+
+      app.use(function (req, res) {
+        res.end(JSON.stringify(req.files))
+      })
+
+      var str = 'x'
       for (var i = 0; i < exp + 1; i += 1) {
-        str += str;
+        str += str
       }
 
       request(app)
-      .post('/')
-      .set('X-OpenRosa-Version', '1.0')
-      .field('user[name]', 'Tobi')
-      .attach('text', new Buffer(str), { filename: 'foo.txt' })
-      .expect(400, done);
+        .post('/')
+        .set('X-OpenRosa-Version', '1.0')
+        .field('user[name]', 'Tobi')
+        .attach('text', new Buffer(str), { filename: 'foo.txt' })
+        .expect(400, done)
     })
 
-    it('should return next(err) if no xml_submission_file is included', function(done) {
-      var app = express();
+    it('should return next(err) if no xml_submission_file is included', function (done) {
+      var app = express()
 
-      app.use(formSubmission());
+      app.use(formSubmission())
 
-      app.use(function(req, res){
-        res.end('whoop');
-      });
+      app.use(function (req, res) {
+        res.end('whoop')
+      })
 
-      app.use(function(err, req, res, next){
-        err.message.should.equal('No xml submission file included in request');
-        res.statusCode = err.status;
-        res.end('bad request');
-      });
+      app.use(function (err, req, res, next) {
+        err.message.should.equal('No xml submission file included in request')
+        res.statusCode = err.status
+        res.end('bad request')
+      })
 
       request(app)
-      .post('/')
-      .set('X-OpenRosa-Version', '1.0')
-      .attach('photo', path.join(__dirname, 'fixtures', 'image.jpg'))
-      .expect(400, 'bad request', done);
+        .post('/')
+        .set('X-OpenRosa-Version', '1.0')
+        .attach('photo', path.join(__dirname, 'fixtures', 'image.jpg'))
+        .expect(400, 'bad request', done)
     })
 
+    it('should reject requests without X-OpenRosa-Version header', function (done) {
+      var app = express()
 
-    it('should reject requests without X-OpenRosa-Version header', function(done) {
-      var app = express();
+      app.use(formSubmission())
 
-      app.use(formSubmission());
-
-      app.use(function(req, res){
-        res.end('whoop');
-      });
+      app.use(function (req, res) {
+        res.end('whoop')
+      })
 
       request(app)
-      .post('/')
-      .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
-      .expect(400, done);
+        .post('/')
+        .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
+        .expect(400, done)
     })
 
-    it('should include "X-OpenRosa-Accept-Content-Length" header in response', function(done) {
-      var app = express();
+    it('should include "X-OpenRosa-Accept-Content-Length" header in response', function (done) {
+      var app = express()
 
-      app.use(formSubmission());
+      app.use(formSubmission())
 
-      app.use(function(req, res){
-        res.end('whoop');
-      });
+      app.use(function (req, res) {
+        res.end('whoop')
+      })
 
       request(app)
-      .post('/')
-      .set('X-OpenRosa-Version', '1.0')
-      .expect('X-OpenRosa-Accept-Content-Length', '10485760')
-      .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
-      .expect(200, done);
-  });
+        .post('/')
+        .set('X-OpenRosa-Version', '1.0')
+        .expect('X-OpenRosa-Accept-Content-Length', '10485760')
+        .attach('xml_submission_file', xmlFixture, { filename: 'foo.xml' })
+        .expect(200, done)
+    })
 
   })
 })
